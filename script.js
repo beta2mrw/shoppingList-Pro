a75aa4596a337d001fe661d4157c90c0532e364d
apple <apple@apples-MacBook-Pro.local>
2023-05-19 11:00:25 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const itemForm = document.getElementById('item-form');
+const itemInput = document.getElementById('item-input');
+const itemList = document.getElementById('item-list');
+
+function addItem(e) {
+  e.preventDefault();
+
+  const newItem = itemInput.value;
+
+  // Validate Input
+  if (newItem === '') {
+    alert('Please add an item');
+    return;
+  }
+  // console.log('Success')
+  // And what we want to do next is get the value that's being typed in, which is going to be this item-input value.
+
+  // create list item
+  const li = document.createElement('li');
+  li.appendChild(document.createTextNode(newItem));
+
+  const button = createButton('remove-item btn-link text-red');
+
+  li.appendChild(button)
+
+  itemList.appendChild(li);
+
+  itemInput.value = '';
+}
+
+function createButton(classes) {
+  const button  = document.createElement('button');
+  button.className = classes;
+  const icon = createIcon('fa-solid fa-xmark');
+  button.appendChild(icon);
+  return button;
+}
+
+function createIcon(classes) {
+  const icon = document.createElement('i');
+  icon.className = classes;
+  return icon;
+}
+
+// Even Listeners
+itemForm.addEventListener('submit', addItem)
